08137e10c26600967f141293517ded17ad75cac4
Alberto Moreno <albertomm@gmail.com>
2016-05-20 20:15:43 +0200

    Solved dice project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+
+  attr_reader :values
+
+  def initialize
+    srand
+  end
+
+  def roll(num)
+    @values = []
+    while num > 0
+      num -= 1
+      @values << rand(6) + 1
+    end
+  end
+
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
